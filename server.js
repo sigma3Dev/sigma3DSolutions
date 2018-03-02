1c13486740c0d6aeade4febd69d53ef80b431a80
Unknown <timwoerner94@web.de>
2018-03-02 16:27:33 +0100

    Convert into JSONRPC and connect to WebSocket

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
-var express     = require('express'),
-    bodyParser  = require('body-parser'),
-    path        = require('path'),
-    app         = express();
+const express     = require('express'),
+      bodyParser  = require('body-parser'),
+      WebSocket   = require('ws'),
+      path        = require('path'),
+      util        = require('util'),
+      app         = express();
+
+const socket = new WebSocket('ws://localhost:8091');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -16,9 +20,51 @@ app.get('/', function(req, res) {
 });
 
 app.post('/calculate-trafo', function(req, res) {
-  console.log(req.body.coords.trafoDataInput);
+  const startPoints = req.body.coords.trafoDataInput.startSystemPoints;
+  const targetPoints = req.body.coords.trafoDataInput.targetSystemPoints;
+
+  let obj = {
+    "jsonrpc": "2.0",
+    "method": "transformation3D6W",
+    "params": {
+      "startPoints": [],
+      "targetPoints": []
+    },
+    "id": 1
+  };
+  
+  startPoints.map(point => {
+    obj.params.startPoints.push({
+      "x": point.x,
+      "y": point.y,
+      "z": point.z
+    })
+  });
+
+  targetPoints.map((point, i) => {
+    obj.params.targetPoints.push({
+      "x": point.x,
+      "y": point.y,
+      "z": point.z,
+      "useX": point['x' + i],
+      "useY": point['y' + i],
+      "useZ": point['z' + i],
+    })
+  });
+
+  socket.onerror = error => {
+    console.log("WebSocket Error: " + error);
+  }
+
+  socket.onmessage = e => {
+    const response = e.data;
+    console.log(response);
+  }
+
+  console.log(util.inspect(obj, false, null));
+  socket.send(JSON.stringify(obj));
 });
 
 app.listen(app.get("port"), () => {
-  console.log("Server running...")
+  console.log("Server is running...");
 });
