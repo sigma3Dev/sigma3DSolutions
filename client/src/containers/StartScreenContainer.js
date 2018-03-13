c7b9a7d9e72d37a319357813a15457b1d2408a51
Unknown <jaanweis@web.de>
2018-03-13 11:31:28 +0100

    Auto stash before merge of "master" and "origin/master"

--- client/src/containers/StartScreenContainer.js
+++ client/src/containers/StartScreenContainer.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import StartScreen from '../components/StartScreen/StartScreen';
-// import {THREE_D_TRANSFORMATION_SEVEN_PARAM} from '../config/AppTypes';
 /**
  * start screen of the app
  * @class StartScreenContainer
