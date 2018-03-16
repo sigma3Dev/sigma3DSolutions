47c81f342e022a593368b9bb76114b0f262604c9
Unknown <svenja.ruthmann@gmx.de>
2018-03-16 09:37:17 +0100

    #51 Icon on Button

--- client/src/components/AppSelectionBtn/AppSelectionBtn.stories.js
+++ client/src/components/AppSelectionBtn/AppSelectionBtn.stories.js
@@ -7,12 +7,11 @@ import { withKnobs, text } from '@storybook/addon-knobs/react';
 import { withInfo } from '@storybook/addon-info';
 
 const label = "Tschebyscheff-Kreis";
-const link = "/geometries/chebyshev_circle_fit"
+const link = "/geometries/chebyshev_circle_fit";
 
 import AppSelectionBtn from './AppSelectionBtn';
 
 const StoryRouter = require('storybook-router');
-
 const stories = storiesOf('Start.AppSelectionBtn', module);
 stories.addDecorator((story, context) => withInfo('common info')(story)(context));
 stories.addDecorator(withKnobs);
