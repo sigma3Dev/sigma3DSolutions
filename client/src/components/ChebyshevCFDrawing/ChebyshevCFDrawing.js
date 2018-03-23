03fc0ab30627eec52e708fff08ee6a216e2f6595
Unknown <jaanweis@web.de>
2018-03-23 08:40:38 +0100

    #57 - edit view of points input in storybook

--- client/src/components/ChebyshevCFDrawing/ChebyshevCFDrawing.js
+++ client/src/components/ChebyshevCFDrawing/ChebyshevCFDrawing.js
@@ -1,28 +1,78 @@
 import React from 'react';
 import './ChebyshevCFDrawing.css';
+//import { object, date } from '@storybook/addon-knobs/dist/react';
 
 /**
  * shows two circles, radius and chebyshev-distance in a svg
+ * hier noch ganz viel schreiben was gemacht wird!!!!!!!!!!!!!!!!!
  * @param {Object} chebyDist - response of chebyshev distance that is returned from backend after successful calculation
  * @param {Object} radius - response of radius that is returned from backend after successful calculation
+ * @param {Object} points - responses an array of points that is returned from backend after successful calculation
  * @returns {*} ChebyshevCFDrawing - .jsx Element
  */
 
 const ChebyshevCFDrawing = ({
   radius, 
-  chebyDist
+  chebyDist,
+  points
 }) => {
+  let shiftedPoints = null;
+  let circlePoints = null;
+  let maßstabKreis=0;
+  if (points !== undefined && points.length > 0) {
+    //maßstabKreis berechnen, 200 ist der Radius bis zum Chebyshev-Kreis
+    maßstabKreis=200/radius;
+    // darstellung der punkte
+    // runde schleife wenn direkt alles returned wird, bei geschweifter extra return statement
+    shiftedPoints = points.map((point, index) => {
+      let xNew=point.x*maßstabKreis+250;
+      let yNew=point.y*maßstabKreis+250;
+        if(index===0){
+          return {x: xNew, y:yNew}//<circle cx={xNew} cy={yNew} r="10" className="cheby-circle-points" />
+        }else{
+          return {x: xNew, y:yNew}//<circle cx={xNew} cy={yNew} r="5" className="cheby-circle-points" />
+        }
+    })
+    circlePoints= shiftedPoints.map((shiftedPoint, index) =>{
+      console.log("iteration:", index+1);
+      let xNew=shiftedPoint.x;
+      let yNew=shiftedPoint.y;
+      console.log("x",xNew);
+      console.log("y",yNew);
+      let abstandZuMP = Math.sqrt((xNew-250)*(xNew-250)+(yNew-250)*(yNew-250));
+      console.log("strecke",abstandZuMP);
+      let alphaWinkel=Math.acos((250-yNew)/abstandZuMP);
+      console.log("winkel",alphaWinkel);
+      let diffZuChebyCircle =abstandZuMP-200;
+      console.log("abstandKreis",diffZuChebyCircle);
+      let maßstabChebyDist=(80/chebyDist)/maßstabKreis;
+      console.log("ms",maßstabChebyDist); 
+        if(index===0){
+          console.log("neuer X-Wert",(xNew-(Math.sin(alphaWinkel)*diffZuChebyCircle))+maßstabChebyDist*(Math.sin(alphaWinkel)*diffZuChebyCircle));
+          return <circle cx={(xNew-(Math.sin(alphaWinkel)*diffZuChebyCircle))+maßstabChebyDist*(Math.sin(alphaWinkel)*diffZuChebyCircle)} cy={(yNew-(Math.cos(alphaWinkel)*diffZuChebyCircle))+maßstabChebyDist*(Math.cos(alphaWinkel)*diffZuChebyCircle)} r="5" fill="blue" />
+        }else{
+          console.log("neuer X-Wert",(xNew-(Math.sin(alphaWinkel)*diffZuChebyCircle))+maßstabChebyDist*(Math.sin(alphaWinkel)*diffZuChebyCircle));
+          console.log("neuer Y-Wert",(yNew-(Math.cos(alphaWinkel)*diffZuChebyCircle))+maßstabChebyDist*(Math.cos(alphaWinkel)*diffZuChebyCircle));
+          return <circle cx={(xNew-(Math.sin(alphaWinkel)*diffZuChebyCircle))+maßstabChebyDist*(Math.sin(alphaWinkel)*diffZuChebyCircle)} cy={(yNew-(Math.cos(alphaWinkel)*diffZuChebyCircle))+maßstabChebyDist*(Math.cos(alphaWinkel)*diffZuChebyCircle)} r="5" className="cheby-circle-points" />
+        }
+    })
+  }
   return(
+    //hier das ist jsx
     <div className="cheby-result-presentation-drawing"> 
       <svg width={500} height={500}>
         <circle cx="250" cy="250" r="240" className="cheby-exterior-circle"/>
         <circle cx="250" cy="250" r="160" className="cheby-interior-circle"/>
-        <line  x1="250"  y1="250" x2="490" y2="250" className="line-cheby-cf"/>
-        <circle cx="250" cy="250" r="3" className="cheby-middle" />
-        <circle cx="410" cy="250" r="3" className="cheby-middle"/>
-        <circle cx="490" cy="250" r="3" className="cheby-middle"/>
+        <circle cx="250" cy="250" r="200" className="chebychev-circle"/>
+        <line  x1="250"  y1="250" x2="450" y2="250" className="line-cheby-cf"/>
         <text x="300" y="240" className="cheby-line-text-radius">{radius}</text>
-        <text x="420" y="240" className="cheby-line-text-chebyDist">{chebyDist}</text>
+        <circle cx="250" cy="250" r="3" className="cheby-points" />
+        <circle cx="450" cy="250" r="3" className="cheby-points"/>
+        <line  x1="250"  y1="410" x2="250" y2="490" className="line-cheby-cf"/>
+        <text x="235" y="400" className="cheby-line-text-chebyDist">{chebyDist}</text>
+        <circle cx="250" cy="410" r="3" className="cheby-points"/>
+        <circle cx="250" cy="490" r="3" className="cheby-points"/>        
+        {circlePoints}
       </svg>
     </div>
   );
--- client/src/components/ChebyshevCFDrawing/ChebyshevCFDrawing.stories.js
+++ client/src/components/ChebyshevCFDrawing/ChebyshevCFDrawing.stories.js
@@ -16,8 +16,8 @@ stories.addDecorator(StoryRouter.default());
 
 stories
   .add('Drawing to display the result of the calculated chebyshev circle fit adjustment especially radius and chebyshev distance', () => {
-    const radius = 81.06;
-    const chebyDist = 3.88;
+    const radius = 81.00.toFixed(2);
+    const chebyDist = 3.88.toFixed(2);
     return(
       <ChebyshevCFDrawing radius={radius} chebyDist={chebyDist} />
     )
@@ -25,9 +25,59 @@ stories
 
 stories
   .add('chebyshev circle with high radius or chebyDist', () => {
-    const radius = number('radius', 1081.06);
-    const chebyDist = number('chebyDist', 13.88);
+    const radius = number('radius', 1081.0.toFixed(2));
+    const chebyDist = number('chebyDist', 13.88.toFixed(2));
     return(
       <ChebyshevCFDrawing radius={radius} chebyDist={chebyDist} />
+    )
+  })
+
+  stories
+  .add('chebyshev circle with points in svg', () => {
+    const radius = number('radius', 1000.00.toFixed(2));
+    const chebyDist = number('chebyDist', 20.00.toFixed(2));
+    const options = {
+      range: true,
+      min: -1000,
+      max: 1000,
+      step: 500,
+    };
+    const defaultValue = 0;
+    const points = [
+      {
+        x: number('x', defaultValue, options),
+        y: number('x', defaultValue, options),
+      },
+      {
+        x: 1000,
+        y: 0,
+      },
+      {
+        x: -1000,
+        y: 0,
+      },
+      {
+        x: 0,
+        y: 1000,
+      },
+      {
+        x: 0,
+        y: -1000,
+      },
+      {
+        x: 0,
+        y: -1010,
+      },
+      {
+        x: 0,
+        y: 990,
+      }
+    ]
+    return(
+      <ChebyshevCFDrawing 
+        radius={radius} 
+        chebyDist={chebyDist} 
+        points={points}
+      />
     )
   })
