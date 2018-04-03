0e0469f34e291ebf8b46d3dcdd552d421fb25838
Unknown <timwoerner94@web.de>
2018-04-03 17:45:51 +0200

    #90 - add errorReducer test

--- client/src/reducers/errorReducer/errorReducer.test.js
+++ client/src/reducers/errorReducer/errorReducer.test.js
@@ -1,5 +1,7 @@
 import { REMOVE_ERROR } from '../../actions/errorHandling/errorHandlingActions';
 import { SUBMIT_3D_TRAFO_COORDS_FAILURE } from '../../actions/submitThreeDTrafoCoords/submitThreeDTrafoCoordsActions';
+import { SUBMIT_CHEBYSHEV_CIRCLE_FIT_COORDS_FAILURE } from '../../actions/submitChebyshevCircleFitCoords/submitChebyshevCircleFitCoordsActions';
+import { SUBMIT_PARAM_INVERSION_COORDS_FAILURE } from '../../actions/paramInversionCoords/paramInversionCoordsActions';
 import errorReducer from './errorReducer';
 
 describe('errorReducer', () => {
@@ -26,6 +28,46 @@ describe('errorReducer', () => {
     };
     const action = {
       type: SUBMIT_3D_TRAFO_COORDS_FAILURE,
+      error: {
+        error: 'Error while trying to fit.',
+      },
+    };
+    const expectedState = {
+      error: {
+        error: 'Error while trying to fit.',
+      },
+    };
+    const result = errorReducer(state, action);
+    expect(result).toEqual(expectedState);
+  });
+  it('should handle SUBMIT_CHEBYSHEV_CIRCLE_FIT_COORDS_FAILURE', () => {
+    const state = {
+      error: {
+        error: null,
+      },
+    };
+    const action = {
+      type: SUBMIT_CHEBYSHEV_CIRCLE_FIT_COORDS_FAILURE,
+      error: {
+        error: 'Error while trying to fit.',
+      },
+    };
+    const expectedState = {
+      error: {
+        error: 'Error while trying to fit.',
+      },
+    };
+    const result = errorReducer(state, action);
+    expect(result).toEqual(expectedState);
+  });
+  it('should handle SUBMIT_PARAM_INVERSION_COORDS_FAILURE', () => {
+    const state = {
+      error: {
+        error: null,
+      },
+    };
+    const action = {
+      type: SUBMIT_PARAM_INVERSION_COORDS_FAILURE,
       error: {
         error: 'Error while trying to fit.',
       },
