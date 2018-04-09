c977c403651942c750fc58006657bfee01ce91c7
Tim <timwoerner94@web.de>
2018-04-09 14:05:56 +0200

    #111 - rename FitPlaneGaussErrorBarGraph to FittingErrorBarGraph

--- client/src/components/FittingErrorBarGraph/FittingErrorBarGraph.stories.js
+++ client/src/components/FittingErrorBarGraph/FittingErrorBarGraph.stories.js
@@ -4,13 +4,13 @@ import { storiesOf } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs/react';
 import { withInfo } from '@storybook/addon-info';
 
-import FitPlaneGaussErrorBarGraph from './FitPlaneGaussErrorBarGraph';
+import FittingErrorBarGraph from './FittingErrorBarGraph';
 
-const stories = storiesOf('Start.FitPlaneGaussErrorBarGraph', module);
+const stories = storiesOf('Start.FittingErrorBarGraph', module);
 stories.addDecorator((story, context) => withInfo('common info')(story)(context));
 stories.addDecorator(withKnobs);
 
 stories.add('Bar graph to display fitting errors for fit plane - DontTest', () => {
   const errors = [1, 5, 4, 5.1, 4.9, 0.1, 3.4, 3.2];
-  return <FitPlaneGaussErrorBarGraph errors={errors} />;
+  return <FittingErrorBarGraph errors={errors} />;
 });
